98e1c03e01259e06517e647ffa120cc643d4f285
Logan Negley <logannegley@gmail.com>
2019-11-02 18:29:47 -0500

    backgorund color change on enter

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+const mainBackground = document.querySelector("body");
+const navBackground = document.querySelector(".main-navigation");
+const footerBackground = document.querySelector(".footer");
+
+document.addEventListener("keypress", function(key) {
+  if (key.keyCode === 13) {
+    mainBackground.style.backgroundColor = "#28fff4";
+    navBackground.style.backgroundColor = "#60ff10";
+    footerBackground.style.backgroundColor = "#fb10ff";
+  } else {
+    alert("Press enter to change background color");
+  }
+});
