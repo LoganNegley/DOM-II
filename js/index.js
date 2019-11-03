2e45688e9f3bf081239e0857fe8436b977962447
Logan Negley <logannegley@gmail.com>
2019-11-02 19:50:32 -0500

    added event to top image and links

--- js/index.js
+++ js/index.js
@@ -13,3 +13,29 @@ document.addEventListener("keypress", function(key) {
     alert("Press enter to change background color");
   }
 });
+
+const firstImg = document.querySelector(".intro img");
+
+firstImg.addEventListener("click", function() {
+  firstImg.style.border = "solid red 4px";
+});
+
+const logo = document.querySelector(".container .logo-heading");
+
+logo.addEventListener("mouseover", () => {
+  logo.style.fontSize = "7rem";
+});
+
+const links = document.querySelectorAll(".nav-link");
+console.log(links);
+links.forEach(function(item) {
+  item.addEventListener("mouseover", () => {
+    item.style.fontSize = "4rem";
+    item.style.color = "red";
+  });
+
+  item.addEventListener("mouseleave", () => {
+    item.style.fontSize = "1.6rem";
+    item.style.color = "black";
+  });
+});
